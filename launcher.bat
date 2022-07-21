21df428e7370b659bd30040b941dd344c8c40571
Krex <102794636+Krex38@users.noreply.github.com>
2022-07-21 18:46:25 +0200

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -3,7 +3,7 @@ echo [40;32mInstalling Requirements For You....[40;37m
 echo.
 pip install --upgrade -r requirements.txt
 echo.
-echo [40;32mLaunching Thunder Builder..[40;37m
+echo [40;32mLaunching Hax Builder..[40;37m
 timeout 2 >nul
 python builder.py
 pause
